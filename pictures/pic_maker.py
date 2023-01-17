6ebea223dd89b7ba7353ff2c20534c8a94d336e7
Andrey Paskov <vbamnup@yandex.ru>
2023-01-17 19:02:48 +0300

    Перенес координаты в config, рефакторинг pic_maker

--- pictures/pic_maker.py
+++ pictures/pic_maker.py
@@ -1,49 +1,67 @@
-import random
+from random import choice
 from datetime import datetime
 from pathlib import Path
 
 from PIL import Image
 
-from settings.config import BACKGROUNDS_DIR, PHRASE_DIR, CALENDAR_DIR
+from settings.config import (
+    BACKGROUNDS_DIR,
+    PHRASE_DIR,
+    CALENDAR_DIR,
+    coord_phrase_left,
+    coord_calendar_left,
+    coord_phrase_right,
+    coord_calendar_right,
+    coord_phrase_center,
+    coord_calendar_center,
+    winter,
+    summer
+)
 
 # Путь к текущему файлу
 # ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 MONTH = datetime.today().month
 
-coord_phrase_center = (80, 350)
-coord_calendar_center = (1030, 175)
 
-coord_phrase_left = (1050, 650)
-coord_calendar_left = (1015, 100)
+class CreateImage:
+    def __init__(self):
+        self.calendar = Image.new('RGB', (1920, 1080))
+        self.random_background = choice(
+            list(Path(BACKGROUNDS_DIR + '/winter').glob('*.*'))
+        )
+        self.random_phrase = choice(list(Path(PHRASE_DIR).glob('*.*')))
+        self.random_calendar = choice(list(Path(CALENDAR_DIR).glob('*.*')))
 
-coord_phrase_right = (115, 655)
-coord_calendar_right = (90, 95)
+    def get_background(self):
+        if MONTH in winter:
+            return Image.open(self.random_background)
+        if MONTH in summer:
+            return Image.open(self.random_background)
+        return Image.open(self.random_background)
 
-if MONTH in [1, 2, 12]:
-    random_background = random.choice(list(Path(BACKGROUNDS_DIR + '/winter').glob('*.*')))
-elif MONTH in [6, 7, 8]:
-    random_background = random.choice(list(Path(BACKGROUNDS_DIR + '/summer').glob('*.*')))
-else:
-    random_background = random.choice(list(Path(BACKGROUNDS_DIR).glob('*.*')))
-random_phrase = random.choice(list(Path(PHRASE_DIR).glob('*.*')))
-random_calendar = random.choice(list(Path(CALENDAR_DIR).glob('*.*')))
+    def get_phrase(self):
+        return Image.open(self.random_phrase)
 
-calendar = Image.new('RGB', (1920, 1080))
-back_image = Image.open(random_background)
-phrase_img = Image.open(random_phrase)
-calendar_img = Image.open(random_calendar)
+    def get_calendar(self):
+        return Image.open(self.random_calendar)
+
+    def make_wallpaper(self):
+        phrase_img = self.get_phrase()
+        calendar_img = self.get_calendar()
+        self.calendar.paste(self.get_background())
+        if 'left' in self.random_background.name:
+            self.calendar.paste(phrase_img, coord_phrase_left, phrase_img)
+            self.calendar.paste(calendar_img, coord_calendar_left, calendar_img)
+        elif 'right' in self.random_background.name:
+            self.calendar.paste(phrase_img, coord_phrase_right, phrase_img)
+            self.calendar.paste(calendar_img, coord_calendar_right, calendar_img)
+        else:
+            self.calendar.paste(phrase_img, coord_phrase_center, phrase_img)
+            self.calendar.paste(calendar_img, coord_calendar_center, calendar_img)
+
+        self.calendar.save('test.png', quality=100)
 
 
-calendar.paste(back_image)
-if 'left' in random_background.name:
-    calendar.paste(phrase_img, coord_phrase_left, phrase_img)
-    calendar.paste(calendar_img, coord_calendar_left, calendar_img)
-elif 'right' in random_background.name:
-    calendar.paste(phrase_img, coord_phrase_right, phrase_img)
-    calendar.paste(calendar_img, coord_calendar_right, calendar_img)
-else:
-    calendar.paste(phrase_img, coord_phrase_center, phrase_img)
-    calendar.paste(calendar_img, coord_calendar_center, calendar_img)
-
-calendar.save('1.png', quality=100)
+a = CreateImage()
+a.make_wallpaper()
--- settings/config.py
+++ settings/config.py
@@ -6,9 +6,25 @@ def get_project_root() -> Path:
     return Path(__file__).parent.parent
 
 
-ROOT_DIR = str(get_project_root())
-TEMPLATES_DIR = '/templates/'
+ROOT_DIR: str = str(get_project_root())
+TEMPLATES_DIR: str = '/templates/'
 
-BACKGROUNDS_DIR = ROOT_DIR + TEMPLATES_DIR + 'backgrounds'
-PHRASE_DIR = ROOT_DIR + TEMPLATES_DIR + 'phrases'
-CALENDAR_DIR = ROOT_DIR + TEMPLATES_DIR + 'calendars'
+BACKGROUNDS_DIR: str = ROOT_DIR + TEMPLATES_DIR + 'backgrounds'
+PHRASE_DIR: str = ROOT_DIR + TEMPLATES_DIR + 'phrases'
+CALENDAR_DIR: str = ROOT_DIR + TEMPLATES_DIR + 'calendars'
+
+
+# Координаты для календаря и цитат
+coord_phrase_center: tuple = (80, 350)
+coord_calendar_center: tuple = (1030, 175)
+
+coord_phrase_left: tuple = (1050, 650)
+coord_calendar_left: tuple = (1015, 100)
+
+coord_phrase_right: tuple = (115, 655)
+coord_calendar_right: tuple = (90, 95)
+
+# Времена года
+winter: tuple = (1, 2, 12)
+summer: tuple = (6, 7, 8)
+
