d48fa62c68465dd94042a55fec5f5ee1f258f2b5
Andrey Paskov <vbamnup@yandex.ru>
2023-02-07 18:06:25 +0300

    Изменение кнопок и логики их появления, теперь можно получить гороскоп и календарь с гороскопом

--- telegram/main.py
+++ telegram/main.py
@@ -1,21 +1,19 @@
 import logging
-from pprint import pprint
-from aiohttp import ClientSession
 
-import requests
 from aiogram import Bot, Dispatcher, executor, types
+from aiohttp import ClientSession
 
 import config
 
 API_TOKEN = config.BOT_API_KEY
 CREATE_USER_URL = config.API_DOMEN + config.USER_CREATE_API
 GET_CALENDAR_URL = config.API_DOMEN + config.CREATE_CALENDAR_API
+GET_MONTHLY_HOROSCOPE = config.API_DOMEN + config.MONTHLY_HOROSCOPE_API
 
 ZODIAC_SINGS = [
-        'Овен', 'Телец', 'Близнецы', 'Рак', 'Лев', 'Дева', 'Весы', 'Скорпион',
-        'Стрелец', 'Козерог', 'Водолей', 'Рыбы'
-    ]
-CREATE_CALENDAR_COMMANDS = ['Создать календарь', 'Календарь с гороскопом']
+    'Овен', 'Телец', 'Близнецы', 'Рак', 'Лев', 'Дева', 'Весы', 'Скорпион',
+    'Стрелец', 'Козерог', 'Водолей', 'Рыбы'
+]
 
 logging.basicConfig(level=logging.INFO)
 
@@ -23,10 +21,41 @@ bot = Bot(API_TOKEN)
 dp = Dispatcher(bot)
 
 
+async def create_start_button(message: types.Message):
+    await bot.send_message(
+        chat_id=message.chat.id,
+        text="Привет, нажми кнопку start, чтобы начать :)",
+        reply_markup=types.ReplyKeyboardMarkup(
+            resize_keyboard=True, keyboard=[
+                [types.KeyboardButton(text="/start")]
+            ]
+        )
+    )
+
+
+async def create_welcome_message(message: types.Message, zodiac=None):
+    text = 'Знак зодиака' if not zodiac else 'Календарь с гороскопом'
+    await bot.send_message(
+        chat_id=message.chat.id,
+        text="Привет, нажми на создать календарь или добавь знак зодиака,"
+             " чтобы получить гороскоп на месяц.",
+        reply_markup=types.ReplyKeyboardMarkup(
+            resize_keyboard=True, keyboard=[
+                [types.KeyboardButton(text=text),
+                 types.KeyboardButton(text="Создать календарь"), ],
+                [types.KeyboardButton(
+                    text="Получить гороскоп"), ] if zodiac else [
+                    types.KeyboardButton(text="/start"), ]
+            ]
+        )
+    )
+
+
 @dp.message_handler(commands='start')
 async def cmd_start(message: types.Message):
     tg_id = message.from_user.id
-    username = message.from_user.first_name
+    username = (message.from_user.username if message.from_user.username
+                else message.from_user.first_name)
 
     data = {
         "tg_id": tg_id,
@@ -39,23 +68,28 @@ async def cmd_start(message: types.Message):
                 logging.info(f'Add user {username}')
             else:
                 logging.error(f'Failed to add user {username}')
-
-    await bot.send_message(
-        chat_id=message.chat.id,
-        text="Привет, нажми на создать календарь или добавь знак зодиака,"
-             " если хочешь, чтобы на календаре был гороскоп на месяц",
-        reply_markup=types.ReplyKeyboardMarkup(
-            resize_keyboard=True, keyboard=[
-                [types.KeyboardButton(text="Знак зодиака"),
-                 types.KeyboardButton(text="Создать календарь"), ]
-            ]
-        )
-    )
+            user_data = await response.json()
+        await create_welcome_message(message, user_data.get('zodiac'))
+    # if not user_data.get('zodiac'):
+    #     await bot.send_message(
+    #         chat_id=message.chat.id,
+    #         text="Привет, нажми на создать календарь или добавь знак
+    #         зодиака,"
+    #              " чтобы получить гороскоп на месяц.",
+    #         reply_markup=types.ReplyKeyboardMarkup(
+    #             resize_keyboard=True, keyboard=[
+    #                 [types.KeyboardButton(
+    #                     text="Знак зодиака" if not user_data.get(
+    #                         'zodiac') else "Получить гороскоп"
+    #                 ),
+    #                     types.KeyboardButton(text="Создать календарь"), ]
+    #             ]
+    #         )
+    #     )
 
 
 @dp.message_handler(lambda message: message.text == 'Знак зодиака')
 async def get_zodiac_buttons(message: types.Message):
-
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
 
     for sign in ZODIAC_SINGS:
@@ -74,7 +108,6 @@ async def add_user_zodiac(message: types.Message):
     sign = message.text
     username = message.from_user.username
     add_zodiac_url = CREATE_USER_URL + f'{tg_id}/zodiac/'
-    print(add_zodiac_url)
 
     data = {
         "zodiac": sign
@@ -91,22 +124,27 @@ async def add_user_zodiac(message: types.Message):
 
     await bot.send_message(
         chat_id=message.chat.id,
-        text="Теперь ты можешь создать календарь со своим гороскопом",
+        text="Теперь ты можешь можешь получить гороскоп!",
         reply_markup=types.ReplyKeyboardMarkup(
             resize_keyboard=True, keyboard=[
                 [types.KeyboardButton(text="Создать календарь"),
-                 types.KeyboardButton(text="Календарь с гороскопом"), ]
+                 types.KeyboardButton(text="Календарь с гороскопом"), ],
+                [types.KeyboardButton(text="Получить гороскоп"), ]
             ]
         )
     )
 
 
-@dp.message_handler(lambda message: message.text in CREATE_CALENDAR_COMMANDS)
+@dp.message_handler(lambda message: message.text == 'Календарь с гороскопом')
 async def create_calendar(message: types.Message):
     tg_id = message.from_user.id
     username = message.from_user.username
     async with ClientSession() as session:
-        async with session.post(GET_CALENDAR_URL + f'?tg_id={tg_id}') as response:
+        async with session.get(CREATE_USER_URL + f'{tg_id}/') as response:
+            data = await response.json()
+        async with session.post(
+                GET_CALENDAR_URL + f'?tg_id={tg_id}',
+                json={'zodiac': data.get('zodiac')}) as response:
             if response.status == 200:
                 logging.info(
                     f'Календарь для пользователя {username} создан'
@@ -115,6 +153,44 @@ async def create_calendar(message: types.Message):
                 await bot.send_photo(chat_id=message.chat.id, photo=file)
             else:
                 logging.error(f'Ошибка создания календаря для {username}')
+
+
+@dp.message_handler(lambda message: message.text == 'Создать календарь')
+async def create_calendar(message: types.Message):
+    tg_id = message.from_user.id
+    username = message.from_user.username
+    async with ClientSession() as session:
+        async with session.post(
+                GET_CALENDAR_URL + f'?tg_id={tg_id}') as response:
+            if response.status == 200:
+                logging.info(
+                    f'Календарь для пользователя {username} создан'
+                )
+                file = await response.read()
+                await bot.send_photo(chat_id=message.chat.id, photo=file)
+            else:
+                logging.error(f'Ошибка создания календаря для {username}')
+
+
+@dp.message_handler(lambda message: message.text == 'Получить гороскоп')
+async def get_monthly_horoscope(message: types.Message):
+    tg_id = message.from_user.id
+
+    async with ClientSession() as session:
+        async with session.post(
+                GET_MONTHLY_HOROSCOPE + f'?tg_id={tg_id}') as response:
+            if response.status == 200:
+                data = await response.json()
+                if data.get('detail') == 'Пользователя нет в базе':
+                    await create_start_button(message)
+                elif data.get('detail') == 'Не заполнен знак зодиака':
+                    await get_zodiac_buttons(message)
+                else:
+                    for cat in data:
+                        await bot.send_message(
+                            chat_id=message.chat.id,
+                            text=f'{cat}\n{data.get(cat)}'
+                        )
 
 
 if __name__ == '__main__':
